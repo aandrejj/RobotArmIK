c55dcc0506ea8c1f8ff67dedea47238df2c68fac
Andrej Pauliny <andrej.pauliny@gmail.com>
2024-02-25 11:29:05 +0100

    BluetoothFactory compilable1

---
+++ RobotArm_MultiServoSweep/BluetoothFactory.cpp
@@ -0,0 +1,106 @@
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "BluetoothFactory.h"
+#include "RemoteController_dataStructures.h"
+
+
+  //unsigned long interval;
+  //unsigned long previousServoMillis;
+  //unsigned long servoInterval;
+  //unsigned long previousSafetyMillis;
+
+int mode;
+int count;
+
+
+//create object
+EasyTransfer ET1;   // send serial
+EasyTransfer ET2;   // rec serial
+
+SEND_DATA_STRUCTURE mydata_send;
+RECEIVE_DATA_STRUCTURE mydata_remote;
+
+SoftwareSerial bluetooth(BLUETOOTH_TX, BLUETOOTH_RX);
+
+	BluetoothFactory::BluetoothFactory() {
+  }
+
+  void BluetoothFactory::begin() {
+    //previousBtMillis = 0;
+    //interval = 20;
+    //previousServoMillis=0;
+    //servoInterval = 200;
+	}
+
+  void BluetoothFactory::BT_to_serial_prepare(){
+    Serial.println("Bluetooth initialization....");
+
+    // Setup BT module
+    pinMode(BLUETOOTH_TX, INPUT);
+    pinMode(BLUETOOTH_RX, OUTPUT);  
+    pinMode(STATE, INPUT);
+    pinMode(ENABLE, OUTPUT);
+    #ifdef EN_PIN_HIGH  
+      digitalWrite(ENABLE, HIGH);   // Used to force AT-mode for HC-05. More flexible is to press the button on the pcb
+    #endif
+    
+    bluetooth.begin(BTBaud);
+    ET1.begin(details(mydata_send), &bluetooth);
+    ET2.begin(details(mydata_remote), &bluetooth);
+    //bluetooth_initialized = true;
+    Serial.println("Bluetooth available.");
+    //previous_Bluetooth_State = bluetooth_On;
+  }
+
+  BluetoothOutputData BluetoothFactory::BT_loop(unsigned long currentMillis) {
+    //unsigned long currentMillis = millis();
+    //if ((currentMillis - BluetoothFactory::previousBtMillis) >= BluetoothFactory::interval) {  
+      // start timed event for read and send
+
+      //BluetoothFactory::previousBtMillis = currentMillis;
+      BluetoothOutputData bluetoothOutputData;
+      if(ET2.receiveData()){                                        // main data receive
+        //BluetoothFactory::previousSafetyMillis = currentMillis; 
+
+        mydata_send.mode  = mode;
+        mydata_send.count = count;
+
+        ET1.sendData();                                           // send data back to remote       
+        /*
+        Serial.println( "LX:"+String(mydata_remote.index_finger_knuckle_right)+
+                      ", LY:"+String(mydata_remote.pinky_knuckle_right)+
+                      ", RX:"+String(mydata_remote.index_finger_fingertip)+
+                      ", RY:"+String(mydata_remote.index_finger_knuckle_left)+
+                      ", count:"+String(count));
+        */
+
+        bluetoothOutputData.dataReceived = true;
+        bluetoothOutputData.index_finger_knuckle_right = mydata_remote.index_finger_knuckle_right;
+        bluetoothOutputData.pinky_knuckle_right = mydata_remote.pinky_knuckle_right;
+        bluetoothOutputData.index_finger_fingertip = mydata_remote.index_finger_fingertip;
+        bluetoothOutputData.index_finger_knuckle_left = mydata_remote.index_finger_knuckle_left;
+
+        /*
+        servo01_constrained = constrain(mydata_remote.index_finger_knuckle_right, 0, 1023);
+        servo02_constrained = constrain(mydata_remote.pinky_knuckle_right, 0, 1023);
+        servo03_constrained = constrain(mydata_remote.index_finger_fingertip, 0, 1023);
+        servo04_constrained = constrain(mydata_remote.index_finger_knuckle_left, 0, 1023);
+        */
+
+        // end of receive data
+      } 
+      //else if(currentMillis - previousSafetyMillis > 200) {         // safeties
+        //Serial.Println("No Data")
+      //}
+
+      count = count + 1;                                              // update count for remote monitoring
+      //return mydata_remote;
+    /*
+    }  // end of timed event Receive/Send
+
+    if (currentMillis - BluetoothFactory::previousServoMillis >= BluetoothFactory::servoInterval) {  // start timed event for Servos  (200 ms)
+      BluetoothFactory::previousServoMillis = currentMillis;
+    }
+    */
+    return bluetoothOutputData;
+}
--- RobotArm_MultiServoSweep/RobotArm_MultiServoSweep.ino
+++ RobotArm_MultiServoSweep/RobotArm_MultiServoSweep.ino
@@ -21,6 +21,15 @@
 #include "LinearRampXYZ.h"
 #include "RampOnce.h"
 #include "RoboArmTurn.h"
+#include "BluetoothFactory.h"
+
+#include "EasyTransfer.h"
+#include "SoftwareSerial.h"
+#include "RemoteController_dataStructures.h"
+#include <Math.h>
+
+
+#include <Wire.h>
 
 #include <Arduino.h>                    // needed for PlatformIO
 #include <SoftwareSerial.h>
@@ -28,10 +37,43 @@
 #include <Ramp.h>
 #include <ArduinoJson.h>  //https://arduinojson.org/   https://arduinojson.org/v6/api/jsonarray/
 
+#define BT_ON  1 ///1 = Bluetooth is on.  0 = Bluetooth is off
+
+/*
+#define IS_HM_10 //uncomment this if module is HM-10
+
+
 #define SERVO_MIN_MILISEC   460                // https://www.arduino.cc/reference/en/libraries/servo/writemicroseconds/
 #define SERVO_MAX_MILISEC  2400                // value of 1000 is fully counter-clockwise, 2000 is fully clockwise, and 1500 is in the middle.
-                                               // ..so that servos often respond to values between 700 and 2300.
+#define SERVO_MIN   135                // https://www.arduino.cc/reference/en/libraries/servo/writemicroseconds/
+#define SERVO_MAX   615                // value of 135 is fully counter-clockwise, 615 is fully clockwise.
+*/
+
+// Outcomment line below for HM-10, HM-19 etc
+//#define HIGHSPEED   // Most modules are only 9600, although you can reconfigure this
+#define EN_PIN_HIGH   // You can use this for HC-05 so you don't have to hold the small button on power-up to get to AT-mode
+
+#ifdef HIGHSPEED
+  #define Baud 38400   // Serial monitor
+  #define BTBaud 38400 // There is only one speed for configuring HC-05, and that is 38400.
+#else
+  #define Baud 9600    // Serial monitor
+  #define BTBaud 9600  // HM-10, HM-19 etc
+#endif
+/*
+
+#define STATE 11
+#define BLUETOOTH_RX 9  // Bluetooth RX -> Arduino D9
+#define BLUETOOTH_TX 10 // Bluetooth TX -> Arduino D10
+//#define GND 13
+//#define Vcc 12
+#define ENABLE 8
+*/
+
+
+                                          // ..so that servos often respond to values between 700 and 2300.
 #define SPEED         30.0                // not a [m/s] but [mm/s]
+
 //#define DEBUG         //debug logging
 #define BRIEF_LOG     //just a few logs
 
@@ -39,6 +81,10 @@ Buttons buttons;
 InverseKinematics inverseKinematics;
 RoboArmTurn roboArmTurn;
 ServosManager servosManager;
+
+BluetoothOutputData bluetoothOutputData;
+BluetoothFactory bluetoothFactory;
+
 LinearRampXYZ linearRampXYZ;
 
 GripPositionXYZ currentGripPosition;
@@ -65,18 +111,41 @@ int servo4PPos = 100;//ruka nabok  100 = zhruba vodorovne
 int servo5PPos = 85; //ruka hore
 int servo6PPos = 90; //ruka otvorena= 100, zatvorena = 60
 
+//SEND_DATA_STRUCTURE mydata_send;
+//RECEIVE_DATA_STRUCTURE mydata_remote;
+
+
 unsigned long currentMillis;
-unsigned long previousMillis;
+//unsigned long previousMillis;
+
+unsigned long previousMillis = 0;
+const long interval = 20;
+
+unsigned long previousBtMillis=0;
+
+unsigned long previousServoMillis=0;
+const long servoInterval = 200;
+
+long previousSafetyMillis;
+
+//SoftwareSerial bluetooth(BLUETOOTH_TX, BLUETOOTH_RX);
 
 //----------------------setup-------------------------------------------
 void setup() {
   // Open serial communications and wait for port to open:
-    Serial.begin(115200);
+  //Serial.begin(115200);
   //Serial.begin( 19200);
   //Serial.begin(  9600);
+  Serial.begin(Baud, SERIAL_8N1);
+
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
+  #ifdef DEBUG 
+    Serial.println(" ");
+    Serial.print("Sketch:   ");   Serial.println(__FILE__);
+    Serial.print("Uploaded: ");   Serial.println(__DATE__);
+  #endif
 
   #ifdef DEBUG 
     Serial.println("RobotArm_servoSweep  Started--------------------------------------------------------");
@@ -84,8 +153,19 @@ void setup() {
   #ifdef DEBUG 
     Serial.println(); 
   #endif
+
+  if(BT_ON ==1) {
+    #ifdef DEBUG 
+      Serial.println(" setup:  Bluetooth is on");
+    #endif
+    bluetoothFactory.begin();
+    bluetoothFactory.BT_to_serial_prepare();
+  } else {
+    Serial.println(" setup:  Bluetooth is off");
+  }
+
   //configure pin 2 as an input and enable the internal pull-up resistor
-  pinMode(2, INPUT_PULLUP);
+  pinMode(53, INPUT_PULLUP);
   pinMode(13, OUTPUT);
   inverseKinematics.begin(SERVO_MIN_MILISEC, SERVO_MAX_MILISEC);
   Serial.println("setup: Setup DONE. System is ready to Initialize servos. Press Button to start...");
@@ -98,12 +178,24 @@ void setup() {
 
 //-----------------loop-------------------------------------------------
 void loop() {
-  boolean Btn2Clicked = buttons.ButtonHandle(2);
+  
+  unsigned long currentMillis = millis();
+
+  if(BT_ON ==1) {
+    if ((currentMillis - previousBtMillis) >= interval) {  
+      bluetoothOutputData = bluetoothFactory.BT_loop(currentMillis);
+      previousBtMillis = currentMillis;
+    }
+  }
+
+
+  boolean Btn2Clicked = buttons.ButtonHandle(53);
   if(Btn2Clicked == true) {
     runRobotArm = buttons.onButton2Clicked(runRobotArm) ;
   }// end of if (Btn2Clicked)
   
   if(runRobotArm == 1) {
+    
     if (initializationDone == 0) {
       #ifdef DEBUG 
 		    Serial.println("loop: Servo Initialization. runRobotArm == 0");
